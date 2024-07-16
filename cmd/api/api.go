e467749ee6b850cc9af35f83f9368e797743d966
root <samspostma@gmail.com>
2024-07-16 18:53:49 +0000

    Init

---
+++ cmd/api/api.go
@@ -0,0 +1,16 @@
+package api
+
+type APIServer struct {
+	addr string
+}
+
+func NewAPIServer(addr string) *APIServer {
+	return &APIServer{
+		addr: addr,
+	}
+}
+
+func (s *APIServer) Run() error {
+	select {}
+	return nil
+}
